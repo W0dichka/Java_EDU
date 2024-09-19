c031a22f503fda7c358141cc791852ad3da35bd3
Kirill Kanashevich <81416978+W0dichka@users.noreply.github.com>
2024-09-19 21:59:15 +0300

    Add files via upload

---
+++ stage_0/stage0-module3-task6/src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -0,0 +1,7 @@
+package lang.print.gaps.task6;
+
+public class FloatTypeCasting {
+    public void roundNumber(float numberToBeRounded) {
+        System.out.println((int) (numberToBeRounded + 0.5));
+    }
+}
