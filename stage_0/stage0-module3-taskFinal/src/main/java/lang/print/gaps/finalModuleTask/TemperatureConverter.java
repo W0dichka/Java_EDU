cac2fa3f967ec5c5862233c5c92ad456ef36d02e
Kirill Kanashevich <81416978+W0dichka@users.noreply.github.com>
2024-09-19 22:02:08 +0300

    Add files via upload

---
+++ stage_0/stage0-module3-taskFinal/src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -0,0 +1,7 @@
+package lang.print.gaps.finalModuleTask;
+
+public class TemperatureConverter {
+    public void toFahrenheit(int temperatureCelsius){
+        System.out.println(((double)temperatureCelsius * 9/5) + 32);
+    }
+}
