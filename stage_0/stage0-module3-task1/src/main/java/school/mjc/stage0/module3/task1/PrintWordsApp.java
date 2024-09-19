be28a4a02e6e5a5762f8bd8a4d487fc17ca8c281
Kirill Kanashevich <81416978+W0dichka@users.noreply.github.com>
2024-09-19 21:42:09 +0300

    Add files via upload

---
+++ stage_0/stage0-module3-task1/src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
@@ -0,0 +1,9 @@
+package school.mjc.stage0.module3.task1;
+
+public class PrintWordsApp {
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("is");
+        System.out.println("smth");
+    }
+}
