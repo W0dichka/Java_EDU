03010596950a9406d32cd555ed634df1aa65ca0d
Kirill Kanashevich <81416978+W0dichka@users.noreply.github.com>
2024-09-19 21:45:00 +0300

    Add files via upload

---
+++ stage_0/stage0-module3-task2/src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
@@ -0,0 +1,14 @@
+package school.mjc.stage0.module3.task2;
+
+public class JavaVariableNames {
+    public static void main(String[] args) {
+        int morrow2 = 0;
+        int $money = 10;
+        int hundred_per = 100;
+        int back_1 = 1000;
+        int _underscore = 10000;
+        int sharp_C = 100000;
+        int star = 1000000;
+        int BADEXAMPLEOFVARNAME = 1000000;
+    }
+}
