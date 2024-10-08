7ddd123d5517e182bed0e3713548be56c9f738b4
Kirill Kanashevich <81416978+W0dichka@users.noreply.github.com>
2024-10-08 15:26:02 +0300

    Add files via upload

---
+++ stage_1/stage1-module1-task2-custom-exception/src/main/java/com/epam/mjc/StudentManager.java
@@ -0,0 +1,31 @@
+package com.epam.mjc;
+
+
+public class StudentManager {
+
+  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+  public static class StudentNotFoundException extends IllegalArgumentException {
+    public StudentNotFoundException(long id) {
+      super("Could not find student with ID " + id);
+    }
+  }
+
+  public Student find(long studentID) {
+      Student student = Student.getValueOf(studentID);
+      if (student == null) {
+          throw new StudentNotFoundException(studentID);
+      }
+      return student;
+  }
+
+  public static void main(String[] args) {
+    StudentManager manager = new StudentManager();
+
+    for (int i = 0; i < IDs.length; i++) {
+      Student student = manager.find(IDs[i]);
+      System.out.println("Student name " + student.getName());
+    }
+
+  }
+}
