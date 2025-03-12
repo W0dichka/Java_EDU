62918827e2ca78d12a8fa10645dc8dcbf752670f
Kirill Kanashevich <81416978+W0dichka@users.noreply.github.com>
2025-03-12 13:58:38 +0300

    Add files via upload

---
+++ stage_0/stage0-module6-task1-/src/main/java/com/epam/mjc/stage0/Bird.java
@@ -0,0 +1,13 @@
+package com.epam.mjc.stage0;
+
+public class Bird extends Animal {
+
+    public Bird() {
+        super("blue", 2, false);
+    }
+    @Override
+    public String getDescription() {
+        String baseDescription = super.getDescription();
+        return baseDescription + " Moreover, it has 2 wings and can fly.";
+    }
+}
